a29783612c6ed1ade13f6866a51dfe109b49fe4a
yagmurcetintas <yagmurcetin@gmail.com>
2019-06-02 19:16:07 -0400

    added initial html structure and linked js and css files

---
+++ assets/game.js
@@ -0,0 +1,11 @@
+window.onload () {
+    
+}
+
+function generateCrystalNumber() {
+    Math.floor(Math.random * 12) + 1;
+}
+
+function generateScoreNumber () {
+    Math.floor(Math.random * 101) + 19;
+}
