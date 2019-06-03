18724afc3dc779167fe423a6398998cffcecc54d
yagmurcetintas <yagmurcetin@gmail.com>
2019-06-03 18:57:00 -0400

    added the logic of game reset

--- assets/game.js
+++ assets/game.js
@@ -11,30 +11,29 @@ $(document).ready(function() {
     generateScoreNumber();
     generateCrystalNumber();
 
-    console.log(redCrystalNumber);
-    console.log(blueCrystalNumber);
-    console.log(yellowCrystalNumber);
-    console.log(purpleCrystalNumber);
-
     $("#redCrystal").on("click", function(){
         totalScore += redCrystalNumber;
         $(".totalScoreDisplay").text(totalScore);
         resultChecker();
+        checkWin();
     });
     $("#blueCrystal").on("click", function(){
         totalScore = totalScore + blueCrystalNumber;
         $(".totalScoreDisplay").text(totalScore);
         resultChecker();
+        checkWin();
     });
     $("#yellowCrystal").on("click", function(){
         totalScore = totalScore + yellowCrystalNumber;
         $(".totalScoreDisplay").text(totalScore);
         resultChecker();
+        checkWin();
     });
     $("#purpleCrystal").on("click", function(){
         totalScore = totalScore + purpleCrystalNumber;
         $(".totalScoreDisplay").text(totalScore);
         resultChecker();
+        checkWin();
     });
 
     function generateCrystalNumber() {
@@ -44,7 +43,9 @@ $(document).ready(function() {
         purpleCrystalNumber = Math.floor(Math.random() * 12) + 1;
         if (redCrystalNumber === blueCrystalNumber || blueCrystalNumber === yellowCrystalNumber || yellowCrystalNumber === purpleCrystalNumber || redCrystalNumber === purpleCrystalNumber || blueCrystalNumber === purpleCrystalNumber || redCrystalNumber === yellowCrystalNumber) {
             generateCrystalNumber();
-        };
+        } else {
+            consoleNumbers();
+        }
     }
 
     function generateScoreNumber() {
@@ -61,13 +62,40 @@ $(document).ready(function() {
             losses++;
             $(".lossesDisplay").text(losses);
             resetGame()
+        } 
+    }
+
+    function checkWin () {
+        if (wins === 10 && losses < 10) {
+            resetGame ();
+            wins = 0;
+            $(".winsDisplay").text(wins);
+            losses = 0;
+            $(".lossesDisplay").text(losses);
+            alert ("Awesome! You won!");
+        } else if (losses === 10 && wins < 10) {
+            resetGame ();
+            wins = 0;
+            $(".winsDisplay").text(wins);
+            losses = 0;
+            $(".lossesDisplay").text(losses);
+            alert ("You lost! But don't give up. Try again!");
         }
     }
+
     function resetGame() {
         totalScore = 0;
         $(".totalScoreDisplay").text(totalScore);
         generateScoreNumber();
         generateCrystalNumber();
+    }
+
+    function consoleNumbers () {
+        console.log("Red Crystal is: " + redCrystalNumber + " points!");
+        console.log("Blue Crystal is: " + blueCrystalNumber + " points!");
+        console.log("Yellow Crystal is: " + yellowCrystalNumber + " points!");
+        console.log("Purple Crystal is: " + purpleCrystalNumber + " points!");
+        console.log("----------------------------");
     }
 });
 
